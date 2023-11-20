d712c310f6c2c46969901bc77ef5006f8d198ac4
Rupa4798 <149367610+Rupa4798@users.noreply.github.com>
2023-11-20 20:53:37 +0530

    Add files via upload

---
+++ Hibernate-4-tableperclass/src/main/java/com/cg/entity/Person.java
@@ -0,0 +1,52 @@
+package com.cg.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.Table;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Table(name="person_join")
+@Inheritance(strategy=InheritanceType.JOINED) 
+
+public class Person {
+	
+	@Id
+	@Column(name="person_id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column(name="person_name")
+	private String name;
+	@Column(name="person_gender")
+	private String gender;
+
+	
+	public Person() {
+		super();
+	}
+	public Person(String name, String gender) {
+		super();
+		this.name = name;
+		this.gender = gender;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String toString() {
+		return id+" "+name+" "+gender;
+	}
+
+}
---
+++ Hibernate-4-tableperclass/src/main/java/com/cg/entity/Student.java
@@ -0,0 +1,48 @@
+package com.cg.entity;
+
+import jakarta.persistence.Column;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="student_join")
+public class Student extends Person{
+	@Column(name="schoolname")
+	private String schoolName;
+	@Column(name="sectionname")
+	private String sectionName;
+	@Column(name="fee")
+	private double fee;
+	public Student(String schoolName, String sectionName, double fee) {
+		super();
+		this.schoolName = schoolName;
+		this.sectionName = sectionName;
+		this.fee = fee;
+	}
+	public Student() {
+		super();
+	}
+	public String getSchoolName() {
+		return schoolName;
+	}
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+	public String getSectionName() {
+		return sectionName;
+	}
+	public void setSectionName(String sectionName) {
+		this.sectionName = sectionName;
+	}
+	public double getFee() {
+		return fee;
+	}
+	public void setFee(double fee) {
+		this.fee = fee;
+	}
+	
+	
+	
+
+}
