31235e5f3d1d57842ee78950fc2e8137a66dab7d
Rupa4798 <149367610+Rupa4798@users.noreply.github.com>
2023-11-20 20:54:02 +0530

    Add files via upload

---
+++ Hibernate-5-join/src/main/java/com/cg/entity/Employee.java
@@ -0,0 +1,53 @@
+package com.cg.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+@Entity
+@Table(name="employee_table_per_class")
+public class Employee extends Person{
+	
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	public double getBonus() {
+		return bonus;
+	}
+	public void setBonus(double bonus) {
+		this.bonus = bonus;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public Employee() {
+		super();
+	}
+	public Employee(double salary, String deptName, double bonus, String emailId) {
+		super();
+		this.salary = salary;
+		this.deptName = deptName;
+		this.bonus = bonus;
+		this.emailId = emailId;
+	}
+	@Column(name="emp_salary")
+	private double salary;
+	@Column(name="emp_deptname")
+	private String deptName;
+	@Column(name="emp_bonus")
+	private double bonus;
+	@Column(name="emp_email")
+	private String emailId;
+	
+	
+}
